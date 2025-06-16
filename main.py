f28d0735e0df213c3c500552ad39fdcba6a7869b
notJunius <120129256+notJunius@users.noreply.github.com>
2025-06-15 19:50:42 -0600

    added pages, but need to fix button

--- main.py
+++ main.py
@@ -9,12 +9,35 @@ def main():
 
     rl.SetTargetFPS(60)
 
+    current_page = "home"
+
+
     while not rl.WindowShouldClose():
+
+        match current_page:
+            case "home":
+                weight_button = rl.GuiButton((100, 100, 100, 50), "Log Weight".encode("utf-8"))
+                if weight_button:
+                    current_page = "weight"
+            case "weight":
+                home_button = rl.GuiButton((100, 100, 100, 50), "Home".encode("utf-8"))
+                if home_button:
+                    current_page = "home"
+                
+
         
 
         rl.BeginDrawing()
-
         rl.ClearBackground(rl.GRAY)
+
+        match current_page:
+            case "home":
+                rl.DrawText("Let's lose some weight you fatass".encode("utf-8"), 400, 400, 32, rl.BLUE)
+            case "weight":
+                rl.DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, rl.BLUE)
+                rl.DrawText("Log your weight today!".encode("utf-8"), 400, 400, 32, rl.GRAY)
+
+
         rl.EndDrawing()
     rl.CloseWindow()
 
