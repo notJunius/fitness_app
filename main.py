cd4647e3950952c1f98b906786a5716b7016d10c
notJunius <120129256+notJunius@users.noreply.github.com>
2025-06-16 12:32:04 -0600

    no more different backgrounds for pages

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def main():
 
         match current_page:
             case "home":
-                weight_button = rl.GuiButton((100, 100, 100, 50), "Log Weight".encode("utf-8"))
+                weight_button = rl.GuiButton((SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/6 * 5, 100, 50), "Log Weight".encode("utf-8"))
                 if weight_button:
                     current_page = "weight"
             case "weight":
-                home_button = rl.GuiButton((100, 100, 100, 50), "Home".encode("utf-8"))
+                home_button = rl.GuiButton((SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/6 * 5, 100, 50), "Home".encode("utf-8"))
                 if home_button:
                     current_page = "home"
                 
@@ -34,8 +34,7 @@ def main():
             case "home":
                 rl.DrawText("Let's lose some weight you fatass".encode("utf-8"), 400, 400, 32, rl.BLUE)
             case "weight":
-                rl.DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, rl.BLUE)
-                rl.DrawText("Log your weight today!".encode("utf-8"), 400, 400, 32, rl.GRAY)
+                rl.DrawText("Log your weight today!".encode("utf-8"), 400, 400, 32, rl.WHITE)
 
 
         rl.EndDrawing()
